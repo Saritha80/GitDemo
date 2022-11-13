a988e771f9aa18f5878354c04aa69e891a84fd08
Saritha80 <93812734+Saritha80@users.noreply.github.com>
2022-11-13 10:57:30 +0530

    Update HelloWorld.java

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -6,7 +6,13 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		String S = "string";
+		S = S+"rtuiuouyut";
 		System.out.println("string S"+S);
+	}
+	
+	public void runable(){
+		system.out.println("new method");
+		
 	}
 
 }
